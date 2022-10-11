0020edc03ce2f55229265ffe476ea87bcdab8c58
ollykr <okremianski@gmail.com>
2022-10-11 17:40:36 -0400

    Direct access to a specific CSS rule

--- 01_04b/script.js
+++ 01_04b/script.js
@@ -2,11 +2,13 @@ const styleSheets = document.styleSheets;
 
 const styleSheetsArray = Array.from(styleSheets);
 const targetSheet = styleSheetsArray.find((styleSheet) => {
-  if (styleSheet?.href) {
-    const pathName = new URL(styleSheet.href).pathname.split("/").pop();
-    if (pathName === "extra.css") {
-      return styleSheet;
-    }
-  }
+	if (styleSheet?.href) {
+		const pathName = new URL(styleSheet.href).pathname.split("/").pop();
+		if (pathName === "extra.css") {
+			return styleSheet;
+		}
+	}
 });
-console.log(targetSheet);
+// just show CSSrules in the DOM [number] indicating index number for the rule
+// .cssText gives me html output of the rule - aka an actual css rules
+console.log(targetSheet.cssRules[0].cssText);
