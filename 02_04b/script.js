97e2fca9f795957f32263d73a477942f7502fcb1
ollykr <okremianski@gmail.com>
2022-10-13 17:08:27 -0400

    Grab a specific style property - e.g bg color

--- 02_04b/script.js
+++ 02_04b/script.js
@@ -1,3 +1,9 @@
+// Access inline CSS styles example
 const listItem = document.querySelector(".animals li:nth-child(5) a");
 
 listItem.style.backgroundColor = "orange";
+
+// Grab all stykes for an element assigned to listItem variable
+console.log(listItem.style);
+// Grab just bg color
+console.log(listItem.style.backgroundColor);
