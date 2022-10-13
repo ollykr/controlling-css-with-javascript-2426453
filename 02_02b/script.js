fc228cfa463f132ce9a80ae36edec7a3fea78e0a
ollykr <okremianski@gmail.com>
2022-10-13 16:52:31 -0400

    Different ways to get the same property

--- 02_02b/script.js
+++ 02_02b/script.js
@@ -6,3 +6,5 @@ const heading = document.querySelector(".masthead h1");
 const computedStyles = window.getComputedStyle(heading);
 // Grab a value for fontSize
 console.log(computedStyles.fontSize);
+// Get computed property value (for a font-size) - it gives the same result as the line above it
+console.log(computedStyles.getPropertyValue("font-size"));
