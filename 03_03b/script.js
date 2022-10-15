979c3cdce69e3f5cab80b55dbe55aeb6f9ee0428
ollykr <okremianski@gmail.com>
2022-10-15 16:00:53 -0400

    Set CSS property using JS-defined and CSS specific methods

--- 03_03b/script.js
+++ 03_03b/script.js
@@ -1,2 +1,14 @@
+// Set existing property value or create a new property - either JS-defined name or CSS specific property name
+
 const styleSheet = document.styleSheets[0];
+// print 70vw
+console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
+
+// Set max-inline-size to 65rem instead of 70vw
+// Method 1 - JS-defined
+styleSheet.cssRules[2].style.maxInlineSize = "65rem";
+// print 65rem
+console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
+// Method 2 - target CSS specific property name
+styleSheet.cssRules[2].style.setProperty("max-inline-size", "65rem");
 console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
