f2f3bb2e1b72028be4236885b94ddbdadf8cfa79
ollykr <okremianski@gmail.com>
2022-10-15 15:24:02 -0400

    Grab specific declaration of cssRule

--- 03_01b/script.js
+++ 03_01b/script.js
@@ -1,1 +1,7 @@
+// Access the declaration of a specific CSS rule
 
+// Grab first stylesheet on a page
+const styleSheet = document.styleSheets[0];
+console.log(styleSheet); /* print style.css */
+// Grab .masthead which is third declaration of a cssRule for style.css
+console.log(styleSheet.cssRules[2]); /* print  selectorText .masthead , etc*/
