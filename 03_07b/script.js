eb359d8e4fba6f866e13915bc4ca781c74c3ec81
ollykr <okremianski@gmail.com>
2022-10-16 16:54:10 -0400

    Play with set/get of a background-color using CSSOM

--- 03_07b/script.js
+++ 03_07b/script.js
@@ -12,3 +12,9 @@ listItem.attributeStyleMap.set("font-size", "4rem");
 console.log(listItem.attributeStyleMap.get("font-size").value);
 // Print "rem"
 console.log(listItem.attributeStyleMap.get("font-size").unit);
+// Print "undefined" - whaaat?!
+console.log(listItem.attributeStyleMap.get("background-color").value);
+// Print CSSStyleValue  - whaaat?!
+console.log(listItem.attributeStyleMap.get("background-color"));
+// The 2 above logs are due to the fact that CSSOM works with values which are numbers , and background-color will never be a number.
+// Although we can set background-color , we won't be able to get its value through CSSOM.
