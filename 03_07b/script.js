0482aa52a8dc1d89ab4d7c82679b8a98fb984d2f
ollykr <okremianski@gmail.com>
2022-10-16 16:47:10 -0400

    Add a font-size using attributeStyleMap

--- 03_07b/script.js
+++ 03_07b/script.js
@@ -1,4 +1,14 @@
+// Create/access inline element styles with attributeStyleMap
+
 const listItem = document.querySelector(".animals li:nth-child(5) a");
 
 listItem.style.backgroundColor = "orange";
 console.log(listItem.style.backgroundColor);
+
+// Add a font-size using styleMap
+listItem.attributeStyleMap.set("font-size", "4rem");
+// Since I have now access to the new attribute, I can get its value
+// Print "4"
+console.log(listItem.attributeStyleMap.get("font-size").value);
+// Print "rem"
+console.log(listItem.attributeStyleMap.get("font-size").unit);
