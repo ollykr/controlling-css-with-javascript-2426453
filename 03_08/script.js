18baaf718377f4bf04317fd830a9b7d6a9c59b3c
ollykr <okremianski@gmail.com>
2022-10-16 18:44:49 -0400

    Solve rotatting box challenge

--- 03_08/script.js
+++ 03_08/script.js
@@ -1,1 +1,13 @@
+// Set initial transition/transform/rotate properties in CSS itself
+// Change properties by injecting inline CSS via JS
 
+const box = document.querySelector(".box");
+const control = document.querySelector("#degrees");
+
+control.addEventListener("change", () => {
+	// Grab a .box , use 'style' interface to set a transform to a new value
+	// within rotate I grab a control value to whatever is changed
+	box.style.transform = `rotate(${control.value}deg)`;
+});
+
+// console.log(styleSheet.cssRules[7].styleMap.get("transition", rotate));
