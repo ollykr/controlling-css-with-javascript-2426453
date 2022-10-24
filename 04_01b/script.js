f3f3386f22f201dcb1e00f28496cba5b8593437b
ollykr <okremianski@gmail.com>
2022-10-24 18:06:55 -0400

    Get a value of a custom property - aka CSS vars

--- 04_01b/script.js
+++ 04_01b/script.js
@@ -1,4 +1,9 @@
 const listItem = document.querySelector(".animals li:nth-child(6) a");
+// Use .getPropertyValue for "--.." values, otherwise, doesn't work in JS
+// We can't do traversing with custom properties
 console.log(listItem.style.getPropertyValue("--color"));
 
 const styleSheet = document.styleSheets[0];
+// Get a value of a property (--max-width) of a first rule (:root) of a first stylesheet
+//  Print 70vw
+console.log(styleSheet.cssRules[0].style.getPropertyValue("--max-width"));
