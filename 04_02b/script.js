61e33d1fa5f03105a2c7af78918a0b2e2708327c
ollykr <okremianski@gmail.com>
2022-10-24 18:12:42 -0400

    Use cssText to get custom property displayed as CSS declaration

--- 04_02b/script.js
+++ 04_02b/script.js
@@ -1,5 +1,11 @@
 const listItem = document.querySelector(".animals li:nth-child(6) a");
-console.log(listItem.style.getPropertyValue("--color"));
+// .cssText gives a verbose version of CSS declaration as it appears either inline or in a stylesheet
+console.log(listItem.style.cssText);
 
 const styleSheet = document.styleSheets[0];
-console.log(styleSheet.cssRules[0].style.getPropertyValue("--max-width"));
+
+// Print
+// --color: hsl(312, 100 %, 50 %);
+// --bg - color: hsl(132, 100 %, 50 %);
+// --max-width: 70vw; --whitespace: 2rem;
+console.log(styleSheet.cssRules[0].style.cssText);
